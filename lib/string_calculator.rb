549bbd09838451d2ad7d5d6f681e0efc7d97fc3e
Brajesh <brajesh112>
2025-11-13 09:48:50 +0530

    Implement two numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,5 +2,7 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.length == 1
+    num1, num2 = numbers.split(",")
+    num1.to_i + num2.to_i
   end
 end
