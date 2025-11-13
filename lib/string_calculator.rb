dcd62ddf60d1f48b7281b7f284679b08535214dd
Brajesh <brajesh112>
2025-11-13 09:53:34 +0530

    Implemented multiple comma seprated numbers sum

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,6 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if numbers.length == 1
-    num1, num2 = numbers.split(",")
-    num1.to_i + num2.to_i
+    numbers.split(",").map(&:to_i).sum
   end
 end
